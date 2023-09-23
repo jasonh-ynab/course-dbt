ca16fa72cdd9ae1b8e58e6e710c25d5b2df2146e
Jason Hill <jasonh@youneedabudget.com>
2023-09-23 14:48:42 +0000

    cleaning up some of intermediate tables based on TA session

--- greenery/models/marts/Product/fct_page_views.sql
+++ greenery/models/marts/Product/fct_page_views.sql
@@ -15,5 +15,24 @@ final as (
         *
     from pageviews
 )
-
+--
+-- final as (
+--    select
+--      date(events.event_created_at_utc) as date
+--      , events.product_id
+--      , events.session_id
+--      , events.user_id
+--      , case
+--          when concat(events.session_id,events.product_id)
+--            = concat(session_order_purchases.session_id,session_order_purchases.product_id) then 1
+--          else 0
+--        end as end_in_purchase
+--    from events
+--    left join
+--      session_order_purchases on
+--      events.session_id =
+--      session_order_purchases.session_id
+--    where event_type = 'checkout'
+--)
+--
 select * from final
--- greenery/models/marts/intermediate/int_session_purchases.sql
+++ greenery/models/marts/intermediate/int_session_purchases.sql
@@ -12,7 +12,7 @@ events as (
 order_items as (select * from {{ ref('stg_postgres__order_items')}}
 ),
 
-session_order_purchases as (
+session_purchases as (
     select
         events.session_id
         , events.user_id
@@ -27,24 +27,4 @@ session_order_purchases as (
     group by 1,2,3,4
 ),
 
-
-final as (
-    select
-      date(events.event_created_at_utc) as date
-      , events.product_id
-      , events.session_id
-      , events.user_id
-      , case
-          when concat(events.session_id,events.product_id)
-            = concat(session_order_purchases.session_id,session_order_purchases.product_id) then 1
-          else 0
-        end as end_in_purchase
-    from events
-    left join
-      session_order_purchases on
-      events.session_id =
-      session_order_purchases.session_id
-    where event_type = 'checkout'
-)
-
-select * from final
+select * from session_purchases
