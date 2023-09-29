79f0d67a3fb2799b7065a06b00b08a269a7938c9
Jason Hill <jasonh@youneedabudget.com>
2023-09-29 16:08:33 +0000

    joining stg products into fct_product_conversions

--- greenery/models/marts/Product/fct_product_conversions.sql
+++ greenery/models/marts/Product/fct_product_conversions.sql
@@ -2,13 +2,22 @@ with sessions as (
     select * from {{ ref('int_sessions')}}
 ),
 
+products as (
+    select * from {{ ref('stg_postgres__products')}}
+),
+
 final as (
     select 
-        distinct(product_id)
-        , count(distinct (session_id))                      as sessions
-        , sum(case when checkouts >= 1 then 1 else 0 end)   as conversions
-        , (conversions / sessions)                          as conversion_rate
-    from sessions
+        distinct(se.product_id)
+        , p.product_name
+        , p.product_price
+        , count(distinct (se.session_id))                      as sessions
+        , sum(case when se.checkouts >= 1 then 1 else 0 end)   as conversions
+        , (conversions / sessions)*100                         as conversion_rate
+        -- I can see that this calculation is not correct, but I don't know WHY - nor how to fix it...
+    from sessions se
+    left join products p
+        on se.product_id = p.product_id
     group by all
 )
 
